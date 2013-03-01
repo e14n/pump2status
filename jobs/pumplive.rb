a75ade06e5ba4ac0b6f19903ee26b68c59ae505a
Evan Prodromou <evan@status.net>
2013-03-01 09:36:07 -0500

    initialize and update once per minute

--- jobs/pumplive.rb
+++ jobs/pumplive.rb
@@ -1,7 +1,10 @@
 current_users = 0
 current_sites = 0
 
-SCHEDULER.every '60s' do
+send_event('users', { current: current_users })
+send_event('sites', { current: current_sites })
+
+SCHEDULER.every '1m' do
   last_users = current_users
   last_sites = current_sites
   current_users = rand(10000)
